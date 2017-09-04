bbfd34ce2aba5d7d7a758a5b59a4cef9c24f25de
Toughee <MJ1menez@protonmail.com>
2017-09-04 01:57:42 -0800

    First commit

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import './App.css';
+import './userId.js'
+
+class App extends Component {
+
+  getDate() {
+    const date = new Date()
+    return date.toTimeString();
+  }
+
+  getBrowserData() {
+    const bData = navigator.userAgent;
+    return bData || 'Sorry. Your Browser Data is unavailable';
+  }
+
+  getOS() {
+    const osSys = navigator.platform;
+    return osSys || 'Your OS information was not found';
+  }
+
+  render() {
+    return (
+      <div className="App animated bounceInUp">
+        <div className="App-header">
+          <h2>Small React app which provides you a brief 
+            summary of your location and device data</h2>
+                <h4>• Your current time is {this.getDate()}</h4>
+                <h4>• Your current browser is {this.getBrowserData()}</h4>
+                <h4>• Your operating system is {this.getOS()}</h4>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/userId.js
@@ -0,0 +1,21 @@
+/* myNetTag Javascript */
+
+/*Current time*/
+/*var date = new Date(); {
+	document.getElementById("timeZone").innerHTML =
+	"• Your current time is " + date.toTimeString();
+}
+/* Current I.P Address*/
+/*function getIP(json) {
+	document.getElementById("ipAddress").innerHTML =
+	"• Your IP address is: " + json.ip;
+}*/
+
+/* O.S */
+/*document.getElementById("operatingSystem").innerHTML =
+"• Your Operating system is " + navigator.platform;*/
+
+/*Browser*/
+/*document.getElementById("browserData").innerHTML =
+"• Your Browser is " + navigator.userAgent;*/
+

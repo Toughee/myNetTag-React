744e695049b690801134b1847aea873138a851c1
Toughee <MJ1menez@protonmail.com>
2017-09-22 11:34:49 -0800

    Fixed layout and fonts

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import './userId.js'
 
+
 class App extends Component {
 
   getDate() {
@@ -18,15 +19,15 @@ class App extends Component {
     const osSys = navigator.platform;
     return osSys || 'Your OS information was not found';
   }
+  
 
   render() {
     return (
       <div className="App animated bounceInUp">
         <div className="App-header">
-          <h2>Small React app which provides you a brief 
-            summary of your location and device data</h2>
+          <h2>| Small React app which provides you a brief 
+            summary of your device data |</h2>
                 <h4>• Your current time is {this.getDate()}</h4>
-                <h4>• Your current browser is {this.getBrowserData()}</h4>
                 <h4>• Your operating system is {this.getOS()}</h4>
         </div>
       </div>

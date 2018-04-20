4d28a23e46264004856d0a9534cadda94c3451de
Toughee <MJ1menez@protonmail.com>
2018-04-19 21:27:48 -0500

    Removed unnecesary files, renamed project, updated favicons, and updated packages

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
-import './userId.js'
+
 
 
 class App extends Component {
@@ -25,8 +25,6 @@ class App extends Component {
     return (
       <div className="App animated bounceInUp">
         <div className="App-header">
-          <h2>| Small React app which provides you a brief 
-            summary of your device data |</h2>
                 <h4>• Your current time is {this.getDate()}</h4>
                 <h4>• Your operating system is {this.getOS()}</h4>
         </div>
